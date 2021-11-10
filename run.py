b76c2dac068284723d66cb8206e13cf505283a09
Martina Grganic <totalnobzvz@yahoo.com>
2021-11-10 16:40:26 +0000

    Add starting functions and initial commit

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def welcome_choice_user():
+    """ Greets the user and offers instructions and options, about, start and quit """
+    pass
+
+
+def questions():
+    """ Asks user to describe objects and asks to set the scene including objects given """
+    pass
+
+
+def information_display():
+    """ Gives information on idea of this game - to engage imagination and inspire people """
+    pass
+
+def display_users_descriptions():
+    """ Displays answers that users gave and clarification of the meaning """
+
+
+def end_game():
+    """ To play again or quit """
+    pass
