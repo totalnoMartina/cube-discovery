ab707914f389edb91358026249f30c4bd7762954
Martina Grganic <totalnobzvz@yahoo.com>
2021-11-21 18:51:08 +0000

    Add a function to hold the interpretation of answers

--- documentation/TESTING.md
+++ documentation/TESTING.md
@@ -1,5 +1,11 @@
 
-# Testing 
+# Testing
+
+While making the function there was a lot of testing in order to cover all validations while user inputs data
+
 ## Bugs
+
+So far, no bugs
+
 ## Test User Stories
 ## Further Testing
--- run.py
+++ run.py
@@ -134,6 +134,7 @@ def choice_answer():
     else:
         print('Not a recognized letter, starting over')
         welcome_choice_user()
+        choice_answer()
 
 
 def next_or_restart():
@@ -150,8 +151,6 @@ def next_or_restart():
         choice_answer()
     
 
-
-
 def welcome_choice_user():
     """ Greets the user, instructions and options, about, start and quit """
     print('''   \033[1mWelcome to the Japanese Cube test!\033[0m\n
@@ -164,122 +163,6 @@ def welcome_choice_user():
     print('This is a game where all answers are right.')
     print(f'Use your imagination, ready {name}?')
     print('Take some time for describing every object...')
-
-#     print('''\n    Now the explanation...
-
-#     The cube is your true self!
-#     If taller than your own height than
-#     your confidence is high while a smaller
-#     one means that you could be shy. If the
-#     cube is moving, it could symbolize a mind
-#     that is open and unconventional while a
-#     still sitting cube is businesslike, a
-#     cube that is dug into the sand means
-#     that you are a planner. A solid cube
-#     means you know who you are, while
-#     a hollow may mean that you are busy
-#     discovering your true self''')
-#     # print(f'    You wrote:{user_answer_first}')
-#     print(40 * '*')
-#     print('''\n
-# The ladder represents two aspects of
-# your life;\n
-# Your goals and frienships.
-# A short ladder represents a small
-# circle of friends and realistic goals,
-# while tall ladder means you have many
-# friends, and your goals are difficult
-# to attain. If leaned onto the cube,
-# shows closeness towards your friends
-# as well as reliability your friends
-# have towrds you, and a ladder equal
-# size as cube shows you that you see
-# your friends as equals in authorithy
-# Lower ladder means you consider
-# yourself as alpha leader of the group
-# of friends. New looking ladder means
-# new friendships are present in your
-# life, while an old looking ladder
-# would imply long enduring friendships''')
-#     # print(f'   You wrote: {user_answer_second}')
-#     print(40 * '*')
-#     # color of the horse - explain
-#     print('''\n
-# Next, the horse; it represents your
-# partner. A horse closely related to
-# cube means you strive for close
-# relationships. And while brown sturdy
-# horse can mean you would like a
-# dependant partner, a glamorous looking
-# horse would mean that you want a
-# partner that will be groomed and
-# approved by others. A horse moving
-# towards the cube would signify
-# a new relationship, while when horse
-# moves away from the cube, means
-# insecurities in relationship.
-# Unicorn/Pegasus mean unrealistic
-# expectations. A brown sturdy working
-# horse means that you want a reliable
-# and stabile partner''')
-#     # print(f'You wrote: "{user_answer_third}"')
-#     print(40 * '*')
-#     print('\n')
-#     print('''
-# Next, the flowers represent your family/
-# children or anything you created and are
-# taking care of. If the flowers are
-# close to the cube, it means you are
-# close to your children. A vibrant
-# blossoming flowers would signify the
-# healthy strong relationship with your
-# kids, while too many flowers could
-# mean you could be overwhelmed with
-# too many concerns around your children.
-# When wilting flowers would represent
-# that relationships are broken or lost.''')
-#     # print(f'    You wrote: "{user_answer_fourth}"')
-#     print(40 * '*')
-#     print('\n')
-#     print('''
-# Next, the thunderstorm represents the
-# stress and fears in your life and
-# how you look at them. A strong storm
-# can mean that the stress really gets
-# to you, and it might be difficult to
-# bounce back. A passing storm in the
-# distance means your stress is visible
-# to you but manageable, while if the
-# storm is affecting any of the objects,
-# you might be overwhelmed by the
-# situation in your life. Just remember,
-# you are bigger than your troubles.
-# If the storm passed through and doesn\'t
-# cause any change, means that you
-# have confidence in resolving your
-# stress.
-# ''')
-    # print(f'    You wrote: "{user_answer_fifth}"')
-
-# if choice == 'q':
-#     print('\x1B[3mYou are quitting, thanks for trying it out!\x1B[0m')
-#     print('Goodbye')
-# elif choice == 'b':
-#     information_display()
-#     print('Would you like to try? (y/n)')
-#     about_choice = input('Enter y or n\n')
-#     if about_choice == 'y':
-#         welcome_choice_user()
-#         choice_answer()
-#     else:
-#         print('Thanks for checking this out!')
-# elif choice == 'n':
-#     print('You want to stop the game?')
-#     print('Thanks for playing!')
-# else:
-#     print('You did not enter a valid letter, let\'s try again')
-#     welcome_choice_user()
-#     choice_answer()
 
 
 def information_display():
@@ -298,11 +181,100 @@ def information_display():
 
 def display_users_descriptions():
     """ Displays answers that users gave and clarification of the meaning """
+    print('''    Now the explanation...
+
+    The cube is your true self!
+    If taller than your own height than
+    your confidence is high while a smaller
+    one means that you could be shy. If the
+    cube is moving, it could symbolize a mind
+    that is open and unconventional while a
+    still sitting cube is businesslike, a
+    cube that is dug into the sand means
+    that you are a planner. A solid cube
+    means you know who you are, while
+    a hollow may mean that you are busy
+    discovering your true self''')
+    print(40 * '*')
+    print('''
+    The ladder represents two aspects of
+    your life;
+    Your goals and frienships. A short ladder represents a small
+    circle of friends and realistic goals, while tall ladder means you
+    have many friends, and your goals are difficult
+    to attain. If leaned onto the cube, shows closeness
+    towards your friends as well as reliability your
+    friends have towrds you, and a ladder equal size as
+    cube shows you that you see your friends as equals
+    in authorithy. Lower ladder means you consider
+    yourself as alpha leader of the group
+    of friends. Brand new looking ladder means
+    new friendships are present in your life, while
+    an old looking ladder would imply long enduring friendships''')
+    # print(f'   You wrote: {user_answer_second}')
+    print(40 * '*')
+    # color of the horse - explain
+    print('''\n
+    Next, the horse; it represents your
+    partner. A horse closely related to
+    cube means you strive for close
+    relationships. And while brown sturdy
+    horse can mean you would like a
+    dependant partner, a glamorous looking
+    horse would mean that you want a
+    partner that will be groomed and
+    approved by others. A horse moving
+    towards the cube would signify
+    a new relationship, while when horse
+    moves away from the cube, means
+    insecurities in relationship.
+    Unicorn/Pegasus mean unrealistic
+    expectations. A brown sturdy working
+    horse means that you want a reliable
+    and stabile partner''')
+    print(40 * '*')
+    print('''
+    Next, the flowers represent your family/
+    children or anything you created and are
+    taking care of. If the flowers are
+    close to the cube, it means you are
+    close to your children. A vibrant
+    blossoming flowers would signify the
+    healthy strong relationship with your
+    kids, while too many flowers could
+    mean you could be overwhelmed with
+    too many concerns around your children.
+    When wilting flowers would represent
+    that relationships are broken or lost.''')
+    print(40 * '*')
+    print('\n')
+    print('''
+    Next, the thunderstorm represents the
+    stress and fears in your life and
+    how you look at them. A strong storm
+    can mean that the stress really gets
+    to you, and it might be difficult to
+    bounce back. A passing storm in the
+    distance means your stress is visible
+    to you but manageable, while if the
+    storm is affecting any of the objects,
+    you might be overwhelmed by the
+    situation in your life. Just remember,
+    you are bigger than your troubles.
+    If the storm passed through and doesn\'t
+    cause any change, means that you
+    have confidence in resolving your
+    stress.''')
 
 
 def all_funcs():
     """ Will be used to run all functions accordingly """
     welcome_choice_user()
+    ask_explain = input('Would you like to see your interpretation? (y/n)')
+    if ask_explain == 'n':
+        print('Thanks for trying this out!')
+    else:
+        display_users_descriptions()
     choice_answer()
     print('You exited the game')
     print('All functions work properly!')
